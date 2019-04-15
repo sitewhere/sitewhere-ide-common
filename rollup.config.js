93ad3cf3b226c0efecef7d65d354dc490275c6dc
Derek Adams <derek.adams@sitewhere.com>
2019-04-15 19:34:01 -0400

    Trying CJS format.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,3 @@
-import resolve from "rollup-plugin-node-resolve";
-import commonJS from "rollup-plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import vue from "rollup-plugin-vue";
 
@@ -8,16 +6,9 @@ export default [
     input: "src/index.ts",
     output: {
       file: "dist/index.js",
-      format: "umd",
+      format: "cjs",
       name: "sitewhere-ide-common"
     },
-    plugins: [
-      resolve(),
-      commonJS({
-        include: "node_modules/**"
-      }),
-      typescript({ clean: true }),
-      vue()
-    ]
+    plugins: [typescript({ clean: true }), vue()]
   }
 ];
